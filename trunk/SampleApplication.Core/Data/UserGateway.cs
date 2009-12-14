f030aa90e0ba4315396277d27d0f61db78eeabac
slmoloch <slmoloch@cff83e8e-e400-11de-a668-6fd11605f5ce>
2009-12-14 16:05:27 +0000

    - core library was added.

---
+++ trunk/SampleApplication.Core/Data/UserGateway.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+
+namespace SampleApplication.Core.Data
+{
+    public class UserGateway
+    {
+        private readonly Configuration config;
+
+        public UserGateway(Configuration config)
+        {
+            this.config = config;
+        }
+
+        public void AddUser(string name, string password)
+        {
+            var doc = Load();
+            AddUser(doc, name, password);
+            Save(doc);
+        }
+
+        public bool IsExists(string name, string password)
+        {
+            return GetAll()
+                .Select(r => r.Name == name && r.Password == password)
+                .Any();
+        }
+
+        public IList<UserRow> GetAll()
+        {
+            IList<UserRow> rows = new List<UserRow>();
+
+            var doc = Load();
+            foreach (XmlNode node in doc.ParentNode.ChildNodes)
+            {
+                rows.Add(new UserRow()
+                             {
+                                 Name = node.Attributes["name"].Value,
+                                 Password = node.Attributes["password"].Value
+                             });
+            }
+
+            return rows;
+        }
+
+        private static void AddUser(XmlDocument doc, string name, string password)
+        {
+            var userElement = doc.CreateElement("user");
+            var nameAttribute = doc.CreateAttribute("name");
+            var passwordAttribute = doc.CreateAttribute("password");
+            userElement.Attributes.Append(nameAttribute);
+            userElement.Attributes.Append(passwordAttribute);
+
+            doc.ParentNode.AppendChild(userElement);
+
+
+            nameAttribute.Value = name;
+            passwordAttribute.Value = password;
+        }
+
+        private void Save(XmlDocument doc)
+        {
+            doc.Save(config.Xml);
+        }
+
+        private XmlDocument Load()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(config.Xml);
+            return doc;
+        }
+    }
+}
+
+
---
+++ trunk/SampleApplication.Core/Data/UserRow.cs
@@ -0,0 +1,10 @@
+﻿namespace SampleApplication.Core.Data
+{
+    public class UserRow
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
+
+
