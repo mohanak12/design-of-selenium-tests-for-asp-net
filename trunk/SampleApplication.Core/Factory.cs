f4fde79c9bdc57d752ded556107be00b543030a0
slmoloch <slmoloch@cff83e8e-e400-11de-a668-6fd11605f5ce>
2009-12-15 13:46:52 +0000

    - storage in xml was added ti store user list.

---
+++ trunk/SampleApplication.Core/Factory.cs
@@ -0,0 +1,12 @@
+﻿using SampleApplication.Core.Data;
+
+namespace SampleApplication.Core
+{
+    public class Factory
+    {
+        public static UserGateway GetGateway()
+        {
+            return new UserGateway(new Configuration());
+        }
+    }
+}
